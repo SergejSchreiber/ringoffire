bc8349bf00a3c15cc5045435a06a005544a77a25
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2023-12-02 20:46:23 +0100

    first playable version

---
+++ src/app/player/player.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player',
+  templateUrl: './player.component.html',
+  styleUrl: './player.component.scss'
+})
+export class PlayerComponent implements OnInit {
+
+  @Input() name!: string;
+  @Input() playerActive: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
