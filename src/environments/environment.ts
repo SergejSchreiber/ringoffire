96ccfeb6e2c3ed9dadff469578e48b104a9e76ab
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2024-01-11 15:58:15 +0100

    update

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDQYqF0IqTUaDtLor4FLGBTqeeBKvuDstU',
+    authDomain: 'ring-of-fire-933ca.firebaseapp.com',
+    projectId: 'ring-of-fire-933ca',
+    storageBucket: 'ring-of-fire-933ca.appspot.com',
+    messagingSenderId: '299324841635',
+    appId: '1:299324841635:web:c8f743b594b1c4c1c66916',
+  },
+};
